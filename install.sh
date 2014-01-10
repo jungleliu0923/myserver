85a900e9eb2c6f0620f343a96b01a3c059840730
jungleliu0923 <liujun0923@zju.edu.cn>
2014-01-10 12:49:20 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh 
+INSTALL_PATH="../"
+MY_LOG_PATH="$INSTALL_PATH/mylog"
+MY_CONF_PATH="$INSTALL_PATH/myconf"
+MY_SERVER_PATH="$INSTALL_PATH/myserver"
+MY_LOG_URL="https://github.com/jungleliu0923/mylog"
+MY_CONF_URL="https://github.com/jungleliu0923/myconf"
+
+if [ -d "$MY_LOG_PATH" ]; then
+	rm -rf "$MY_LOG_PATH"
+fi
+
+if [ -d "$MY_CONF_PATH" ]; then
+	rm -rf "$MY_CONF_PATH"
+fi
+
+git clone "$MY_LOG_URL"  "$MY_LOG_PATH"
+git clone "$MY_CONF_URL" "$MY_CONF_PATH"
+
+cd "$MY_LOG_PATH" && make
+cd  "$MY_CONF_PATH" && make
+make
+make -C sample
+
